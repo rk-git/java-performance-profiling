d205f74a66ee3eb618ac77e91d2658c328e50f04
Rk Somasundaram <rk@tigase.net>
2025-09-21 20:36:21 -0700

    Code to demonstrate thred starvation through JMC

--- src/main/java/org/rks/java/performance/StarvationDemo.java
+++ src/main/java/org/rks/java/performance/StarvationDemo.java
@@ -1,17 +1,50 @@
 package org.rks.java.performance;
 
-import java.util.concurrent.*;
+import jdk.jfr.Category;
+import jdk.jfr.Event;
+import jdk.jfr.Label;
+import jdk.jfr.Registered;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class StarvationDemo {
     public static final int WAIT_TIME = 30000;
+
+    @Category({"StarvationDemo", "Events"})
+    @Label("NestedTaskSubmitted")
+    @Registered(true) // ← 🔥 This makes it show up in JMC by default
+    static class NestedTaskSubmittedEvent extends Event {
+        @Label("Thread")
+        String threadName;
+
+        @Label("Time")
+        long timestamp;
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService pool = Executors.newFixedThreadPool(2);
+        ExecutorService pool = Executors.newFixedThreadPool(2, factory -> {
+            Thread thread = new Thread(factory);
+            // Name the threads so the threads are conspicuous in JMC
+            thread.setName("starving-thread-" + UUID.randomUUID());
+            return thread;
+        });
 
         // The following task will be submitted and th etask will
         // invoke Future.get(), thereby blocking itself, since the executor has only
         // 2 threads.
         Runnable blockingTask = () -> {
-            System.out.println(Thread.currentThread().getName() + ": Submitting nested task...");
+            final String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + ": Submitting nested task...");
+
+            NestedTaskSubmittedEvent evt = new NestedTaskSubmittedEvent();
+            evt.threadName = threadName;
+            evt.timestamp = System.currentTimeMillis();
+            evt.commit();  // 🔴 appears in JFR timeline!
+
+
             Future<?> nested = pool.submit(() -> {
                 System.out.println(Thread.currentThread().getName() + ": Running nested task");
             });
@@ -26,7 +59,7 @@ public class StarvationDemo {
 
         // Following two submissions consue the two threas, thereby
         // making no threads available ot the inner tasks when it does Future.get(), causing
-	// thread starvation. We can use JMC to observe this.
+        // thread starvation. We can use JMC to observe this.
         pool.submit(blockingTask);
         pool.submit(blockingTask);
 
